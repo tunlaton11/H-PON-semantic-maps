d54b20853ebf1173c0159bb98401ab745f24fca7
Thanapat Teerarattanyu <37036113+tjthanapat@users.noreply.github.com>
2023-03-20 21:49:33 +0700

    create configuration

---
+++ configs/config_utilities.py
@@ -0,0 +1,18 @@
+from yacs.config import CfgNode
+
+
+def load_config(config_path: str) -> CfgNode:
+    """Load configurations from a YAML file.
+
+    Parameters
+    ----------
+    config_path : str
+        Path of YAML file
+
+    Returns
+    -------
+    CfgNode
+        Configurations
+    """
+    with open(config_path) as f:
+        return CfgNode.load_cfg(f)
