08b92947ef043ae3b1f28eb59290c16513123db5
tunlaton11 <tunlaton11@gmail.com>
2023-08-29 14:00:16 +0700

    add torch utils

---
+++ utilities/torch.py
@@ -0,0 +1,15 @@
+import torch
+import platform
+import re
+
+def detect_device():
+    this_device = platform.platform()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        device = "cuda"
+    elif re.search("arm64", this_device):
+        # use Apple GPU
+        device = "mps"
+    else:
+        device = "cpu"
+    return device
