a8d176de7fc220710e2ae60893ec035ef0449179
tunlaton11 <tunlaton11@gmail.com>
2023-09-15 00:29:06 +0700

    add saving checkpoint every n epochs

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-from configs.config_utilities import load_config
+import torch
+import torch.optim as optim
 from torch.utils.data import DataLoader
+import albumentations as A
+import numpy as np
+from tqdm import tqdm
+import os
+import time
 
+from configs.config_utilities import load_config
 from models.pyramid import build_pon, build_hpon
 from dataset import NuScenesDataset
-
 from criterion import OccupancyCriterion
 from logger import TensorboardLogger
 import utilities.torch as torch_utils
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-
-import os
-import time
-from tqdm import tqdm
-import numpy as np
-
 
 def main():
+    ## SET EXPERIMENT CONFIG ##
     config = load_config("configs/configs.yml")
+    experiment_title = f"Test_{config.network}_{config.nuscenes_version}_{time.time()}"
+    is_load_checkpoint = False
+    if is_load_checkpoint:
+        # set experiment title and checkpoint path manuanlly here.
+        experiment_title = "[network]_[nuscenes_version]_[unix_timestamp]"
+        load_checkpoint_epoch = 99  # epoch to load checkpoint
+        load_checkpoint_path = (
+            f"{config.checkpoint_dir}/{experiment_title}"
+            + f"/{experiment_title}_{str(epoch).zfill(load_checkpoint_epoch)}.pt"
+        )
 
     train_transform = A.Compose(
         [
@@ -50,7 +56,6 @@ def main():
         nuscenes_dir=config.nuscenes_dir,
         nuscenes_version=config.nuscenes_version,
         label_dir=config.label_dir,
-        # sample_tokens=config.train_tokens,
         sample_tokens=np.loadtxt("configs/mini_train_sample_tokens.csv", dtype=str),
         image_size=(200, 112),
         transform=train_transform,
@@ -67,7 +72,6 @@ def main():
         nuscenes_dir=config.nuscenes_dir,
         nuscenes_version=config.nuscenes_version,
         label_dir=config.label_dir,
-        # sample_tokens=config.val_tokens,
         sample_tokens=np.loadtxt("configs/mini_val_sample_tokens.csv", dtype=str),
         image_size=(200, 112),
     )
@@ -80,9 +84,14 @@ def main():
     )
 
     device = torch_utils.detect_device()
+    print(f"Training on {device}")
 
-    # network = build_pon(config).to(device)
-    network = build_hpon(config, htfm_method="stack").to(device)
+    if config.network == "H-PON":
+        network = build_hpon(config, htfm_method="stack").to(device)
+    elif config.network == "PON":
+        network = build_pon(config).to(device)
+    else:
+        raise "Only H-PON and PON options available for network"
 
     # criterion = nn.BCEWithLogitsLoss().to(device)
     criterion = OccupancyCriterion(
@@ -95,28 +104,18 @@ def main():
 
     optimizer = optim.Adam(network.parameters(), lr=config.lr)
 
-    is_load_checkpoint = False
+    log_dir = f"{config.log_dir}/{experiment_title}"
+    num_epochs = config.epochs
     if is_load_checkpoint:
-        experiment_title = "Full_EPON_H-collage_1693459089.077859"
-        log_dir = f"runs/{experiment_title}"
-        checkpoint_path = f"checkpoints/{experiment_title}/Full_EPON_H-collage_1693459089.077859_00099.pt"
-        checkpoint = torch.load(checkpoint_path)
+        print(f"Loading checkpoint from {load_checkpoint_path}")
+        checkpoint = torch.load(load_checkpoint_path)
         network.load_state_dict(checkpoint["model_state_dict"])
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
         initial_step = checkpoint["step"]
         initial_epoch = checkpoint["epoch"] + 1
-        epochs = initial_epoch + 200
     else:
-        current_time = time.time()
-        # experiment_title = f"Full_HPON_H-collage_{current_time}"
-        experiment_title = f"Full_HPON_H-stack_{current_time}"
-        # experiment_title = f"Full_PON_{current_time}"
-        log_dir = f"{config.log_dir}/{experiment_title}"
         initial_step = 0
         initial_epoch = 0
-        epochs = config.epochs
-
-    network.to(device)
 
     logger = TensorboardLogger(
         device=device,
@@ -160,7 +159,7 @@ def main():
             "Experiment Configurations", config_log_table, global_step=0
         )
 
-    for epoch in tqdm(range(initial_epoch, epochs)):
+    for epoch in tqdm(range(initial_epoch, num_epochs)):
         for batch in train_loader:
             images, labels, masks, calibs = batch
             images = images.to(device)
@@ -185,44 +184,47 @@ def main():
 
         logger.log_epoch(network, epoch)
 
-        # save best model
-        # if logger.save_model:
-        #     print(f"save model at epoch {epoch}")
-        #     checkpoint_dir = os.path.expandvars(config.checkpoint_dir + "/" + experiment_title)
-        #     os.makedirs(checkpoint_dir, exist_ok=True)
-
-        #     checkpoint_path = (
-        #         checkpoint_dir + f"/{experiment_title}_{str(epoch).zfill(5)}_best.pt"
-        #     )
-
-        #     torch.save(
-        #         dict(
-        #             epoch=epoch,
-        #             step=logger.training_step,
-        #             model_state_dict=network.state_dict(),
-        #             optimizer_state_dict=optimizer.state_dict(),
-        #         ),
-        #         checkpoint_path,
-        #     )
-
-        # early stop
-        # if logger.not_improve_consec_counter == 10:
-        #     print(f"stop training at epoch {epoch}")
-        #     break
+        # save checkpoint every n epochs
+        if (
+            epoch % config.num_epochs_to_save_checkpoint
+            == config.num_epochs_to_save_checkpoint - 1
+        ):
+            print(f"Saving model at epoch {epoch}")
+            checkpoint_dir = f"{config.checkpoint_dir}/{experiment_title}"
+            os.makedirs(checkpoint_dir, exist_ok=True)
+            checkpoint_path = (
+                checkpoint_dir + f"/{experiment_title}_{str(epoch).zfill(5)}.pt"
+            )
+            torch.save(
+                dict(
+                    epoch=epoch,
+                    step=logger.training_step,
+                    model_state_dict=network.state_dict(),
+                    optimizer_state_dict=optimizer.state_dict(),
+                ),
+                checkpoint_path,
+            )
 
     # save last epoch
-    checkpoint_dir = os.path.expandvars(config.checkpoint_dir + "/" + experiment_title)
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_path = checkpoint_dir + f"/{experiment_title}_{str(epoch).zfill(5)}.pt"
-    torch.save(
-        dict(
-            epoch=epoch,
-            step=logger.training_step,
-            model_state_dict=network.state_dict(),
-            optimizer_state_dict=optimizer.state_dict(),
-        ),
-        checkpoint_path,
-    )
+    if (
+        epoch % config.num_epochs_to_save_checkpoint
+        != config.num_epochs_to_save_checkpoint - 1
+    ):
+        print(f"Saving model at epoch {epoch}")
+        checkpoint_dir = f"{config.checkpoint_dir}/{experiment_title}"
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        checkpoint_path = (
+            checkpoint_dir + f"/{experiment_title}_{str(epoch).zfill(5)}.pt"
+        )
+        torch.save(
+            dict(
+                epoch=epoch,
+                step=logger.training_step,
+                model_state_dict=network.state_dict(),
+                optimizer_state_dict=optimizer.state_dict(),
+            ),
+            checkpoint_path,
+        )
 
 
 if __name__ == "__main__":
